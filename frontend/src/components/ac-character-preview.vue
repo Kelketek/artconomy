bb61e2d2aa38daee6ffe4a21ec1269639de3f2c0
Fox Piacenti <fox@vulpinity.com>
2018-02-21 14:40:07 -0600

    Tweak character appearance. Need Django 2.0 now.

--- frontend/src/components/ac-character-preview.vue
+++ frontend/src/components/ac-character-preview.vue
@@ -1,28 +1,30 @@
 <template>
-  <div class="col-12-sm col-4-md col-3-lg col-centered character-preview pr-2 pl-2 mb-3 clickable">
-    <div class="card character-card">
-      <div class="bg-primary card-header character-name-tag fg-light">
+  <v-flex xs12 md4 lg3 class="character-preview clickable">
+    <v-card class="card character-card">
+      <v-card-media
+          :src="$img(character.primary_asset, 'thumbnail')"
+      >
+        <router-link :to="{name: 'Character', params: {username: character.user.username, characterName: character.name}}">
+          <ac-asset
+              :asset="character"
+              thumb-name="thumbnail"
+              :terse="true"
+              :text-only="true"
+          />
+        </router-link>
+      </v-card-media>
+      <v-card-title>
         {{ character.name }}
         <span v-if="removable" @click="remove"><i class="fa fa-times"></i></span>
-      </div>
-      <router-link :to="{name: 'Character', params: {username: character.user.username, characterName: character.name}}">
-        <ac-asset
-            :terse="true"
-            thumb-name="thumbnail"
-            v-if="character.primary_asset && character.primary_asset.id"
-            :asset="character.primary_asset"
-            img-class="card-img-top"
-        />
-        <img v-else class="card-img-top" src="/static/images/default-avatar.png" />
-      </router-link>
-    </div>
+      </v-card-title>
+    </v-card>
     <div class="showcase-button-container" v-if="canShowcase">
       <ac-action :url="url" variant="primary" :success="callback" :disabled="showcased">
         <span v-if="showcased" :button="true">Showcased</span>
         <span v-else>Showcase</span>
       </ac-action>
     </div>
-  </div>
+  </v-flex>
 </template>
 
 <style>
