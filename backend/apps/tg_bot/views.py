f2ac929c671e65e3b5dd510bb8b49edfb1414f5a
Fox Piacenti <fox@vulpinity.com>
2018-06-21 11:36:54 -0500

    Fixes for bot.

--- backend/apps/tg_bot/views.py
+++ backend/apps/tg_bot/views.py
@@ -3,15 +3,23 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from telegram import Update
-from telegram.ext import Dispatcher
 
-from apps.lib.utils import BOT
+from apps.tg_bot.bot import init
+
+UPDATER = None
+
+
+def get_updater():
+    global UPDATER
+    if UPDATER is None:
+        UPDATER = init(settings.TELEGRAM_BOT_KEY)
+    return UPDATER
 
 
 class ProcessUpdate(APIView):
     def post(self, request, secret):
         if secret != settings.TELEGRAM_BOT_KEY:
             return Response(status=status.HTTP_403_FORBIDDEN, data={'error': 'Incorrect key.'})
-        dispatcher = Dispatcher(BOT, None, workers=0)
-        dispatcher.process_update(Update.de_json(request.data, BOT))
+        updater = get_updater()
+        updater.dispatcher.process_update(Update.de_json(request.data, updater.bot))
         return Response(status=status.HTTP_204_NO_CONTENT)
