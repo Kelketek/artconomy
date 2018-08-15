0c55d2a9838d5b01999aed616ed8cd196e76e5d3
Fox Piacenti <fox@vulpinity.com>
2018-08-14 19:27:13 -0500

    Slightly reduce dependence on jQuery.

--- frontend/src/lib.js
+++ frontend/src/lib.js
@@ -12,7 +12,7 @@ export function getCookie (name) {
   if (document.cookie && document.cookie !== '') {
     const cookies = document.cookie.split(';')
     for (let i = 0; i < cookies.length; i += 1) {
-      const cookie = $.trim(cookies[i])
+      const cookie = cookies[i].trim()
       // Does this cookie string begin with the name we want?
       if (cookie.substring(0, name.length + 1) === (`${name}=`)) {
         cookieValue = decodeURIComponent(cookie.substring(name.length + 1))
