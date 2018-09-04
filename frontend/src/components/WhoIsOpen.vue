0be13e85d5ab5b17a5b25d3892762981587f9ef3
Fox Piacenti <fox@vulpinity.com>
2018-09-04 15:43:28 -0500

    Refactor front page listings.

--- frontend/src/components/WhoIsOpen.vue
+++ frontend/src/components/WhoIsOpen.vue
@@ -1,10 +1,10 @@
 <template>
   <v-container grid-list-md>
-    <v-tabs v-model="tab" fixed-tabs class="mb-2">
+    <v-tabs v-model="tab" fixed-tabs class="mb-2" v-if="isLoggedIn">
       <v-tab href="#tab-watchlist">Watchlist</v-tab>
       <v-tab href="#tab-all">All</v-tab>
     </v-tabs>
-    <v-tabs-items v-model="tab">
+    <v-tabs-items v-model="tab" v-if="isLoggedIn">
       <v-tab-item id="tab-watchlist">
         <store class="pt-2" endpoint="/api/sales/v1/who-is-open/" counter-name="watchlist-open" :show-error="true" empty-error="No one on your watchlist is currently open." />
       </v-tab-item>
@@ -12,18 +12,21 @@
         <store class="pt-2" endpoint="/api/sales/v1/new-products/" :show-error="true" empty-error="There are no products currently open." />
       </v-tab-item>
     </v-tabs-items>
+    <store class="pt-2" endpoint="/api/sales/v1/new-products/" :show-error="true" empty-error="There are no products currently open." v-else />
   </v-container>
 </template>
 
 <script>
   import Store from './Store'
   import Paginated from '../mixins/paginated'
+  import Perms from '../mixins/permissions'
+  import Viewer from '../mixins/viewer'
   import {paramHandleMap, EventBus} from '../lib'
 
   export default {
     name: 'WhoIsOpen',
     components: {Store},
-    mixins: [Paginated],
+    mixins: [Paginated, Perms, Viewer],
     methods: {
       resultCheck (data) {
         if (data.name === 'watchlist-open') {
