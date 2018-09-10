a7f33264ff01b473ec96f5998ff473c6d2683ad1
Fox Piacenti <fox@vulpinity.com>
2018-09-10 14:23:37 -0500

    Update Commission Agreement.

--- frontend/src/components/CommissionAgreement.vue
+++ frontend/src/components/CommissionAgreement.vue
@@ -10,62 +10,72 @@
         </div>
       </div>
       <div class="col-12-sm">
-        <p><strong>Last updated: July 03, 2018</strong></p>
+        <p><strong>Last updated: September 10, 2018</strong></p>
         <p>
-          This is an agreement between commissioner, artist, and the intermediary, henceforth known as Artconomy.
+          This is an agreement between the commissioner (“Commissioner”), artist (“Artist”), and the intermediary (“Artconomy”) to complete an artistic work ("Commission").
         </p>
         <h2>Basics</h2>
         <p>
-          The artist will, to their abilities and earnest interpretation, render a work or works as specified in the product description (henceforth known as the deliverables) and the commissioner's notes. The artist is expected to have read and fully understood, to the best of their ability, the requirements of the commissioner before accepting the commission.
+          Artconomy's escrow and dispute resolution service will be known as "Artconomy Shield".
         </p>
         <p>
-          The commissioner will pay, up front, the amount agreed upon in the order form. This amount will be held in escrow by Artconomy until the work is completed, if the product is marked as covered by the Artconomy Shield escrow service.
+          The Artist will, to their abilities and earnest interpretation, render a work or works as specified in the product descriptions and the Commissioner's notes (the “Deliverables”). The Artist is expected to have read and fully understood, to the best of their ability, the requirements of the Commissioner before accepting the commission.
         </p>
         <p>
-          If Arconomy Shield is used, Artconomy will release, upon completion of the project and approval of the commissioner (or a period of ten days after the deliverables are posted if no dispute is filed) the funds, minus their fees as scheduled with the artist to the artist.
+          The Commissioner will pay, up front, the amount agreed upon in the order form. If the product is marked as covered by the Artconomy Shield escrow service, this amount (the “Escrow Amount”) will be held in escrow by Artconomy until the work is completed.
+        </p>
+        <p>
+          If Arconomy Shield is used, Artconomy will release to the Artist the Escrow Amount, less its fees as scheduled with the Artist (the “Fees”), either (a) within 10 business days after the Commissioner approves the Deliverables; or (b) in the event neither an affirmative approval is given nor a dispute is filed within a period of ten days after the Deliverables are posted (the “Review Period”), within 10 business days after the expiration of the Review Period.
         </p>
         <h2>Revisions</h2>
         <p>
-          The artist may offer opportunities for note and revision (henceforth known as revisions). The number of revisions will be specified upon order, and shall be scoped to reasonable changes at the point of the deliverable's progress. If the commissioner does not give notes for revision after five days, the commissioner waives the right to offer revision notes and the artist may continue as though approval were explicitly given.
+          The Artist may offer opportunities for note and revision (“Revisions”), but if and only if the order provides for Revisions and, in that case, only so many Revisions as the order specifies. The Revisions shall be scoped to reasonable changes at the point of the deliverable's progress, such as altering linework on an uncolored revision, or changing flat colors if the medium permits. If the commissioner does not give notes for revision after five days of the revision's posting, the Commissioner waives the right to offer revision notes and the Artist may continue as though approval were explicitly given.
         </p>
         <h2>Disputes</h2>
-        <p>
-          At a time equal to 1.25x times the estimated turnaround time, (in business days), rounded up to the next day, after the order is placed or after a final is delivered to the commissioner and before the commissioner's final approval, the commissioner may file a dispute with Artconomy if the product is protected by Artconomy Shield. Artconomy will review the order agreement and history, and will determine how to resolve the dispute. Artconomy may, at its discretion, refund all or part of the price payed by the commissioner if they find in favor of the commissioner. The artist may be required to cover the transaction fees involved in refunding the customer.
-        </p>
+        <p>For works covered by Artconomy Shield, a dispute may be filed by the commissioner:</p>
+        <ul>
+          <li>At a time equal to 1.25x times the estimated turnaround time, (in business days), rounded up to the next day, after the order is placed</li>
+          <li>After the final Deliverable is delivered to the commissioner</li>
+        </ul>
         <p>
           Examples of reasons for filing a dispute include but are not limited to:
         </p>
         <ul>
           <li>Artist has taken longer than is reasonable to complete deliverables</li>
-          <li>The deliverables deviate from the product's description and the customer's provided details for the order, as could be reasonably understood</li>
+          <li>The Deliverables deviate from the product's description and the Commissioner provided reasonably understandable details for the order.</li>
         </ul>
         <p>
-          The artist and the commissioner both agree that no damages will be sought over the amount paid for the commission. Commissions are to be considered 'as-is', and not to be warranted for any specific purpose.
+          The Commissioner and Artist agree that Artconomy’s determination in any dispute is binding and final. The Artist and the Commissioner further agree not to seek any damages from each other related to the Deliverables. Deliverables are to be considered 'as-is', and not to be warranted for any specific purpose.
         </p>
         <p>
-          The commissioner and artist agree to consider the determination of Artconomy in disputes to be binding and final.
-        </p>
-        <p>
-          Orders not protected by Artconomy Shield (including any orders which have a price of $0.00), will not use Artconomy's escrow or dispute resolution services. Artists and commissioners not using Artconomy Shield will assume all risks involved in their transactions between one another.
+          In orders not covered by Artconomy Shield (including orders for which the price is $0.00), the Artist and Commissioner will assume all risks involved in their transactions between one another.
         </p>
         <h2>Intellectual Property and Liability</h2>
         <p>
-          The commissioner hereby certifies that they hold or have otherwise obtained any required permissions or licensing to use any trademark, copyrighted work or other form of intellectual property requested for the deliverable, and that the work being commissioned is otherwise legal to purchase within the legal framework to which they are subjected. The commissioner assumes liability for these licenses and further certifies that neither Artconomy nor the Artist shall be held liable if these requirements have not been met.
+          The commissioner hereby certifies that they hold or have otherwise obtained any required permissions or licensing to use any trademark, copyrighted work or other form of intellectual property requested for the deliverable, and that the work being commissioned is otherwise legal to purchase within the legal framework to which they are subjected. The commissioner assumes liability for these licenses and indemnifies Artconomy and the artist from any and all losses related to errors or misrepresentation in this licensing.
         </p>
         <p>
-          The artist hereby certifies that the tools that they possess have been obtained by legal means and that any licenses required for their use have been properly purchased and kept up to date. The artist further certifies that any stock images or data which are used to create the deliverables has been properly licenced and is legal to use within the legal framework to which they are subjected.
+          The artist hereby certifies that the tools that they possess have been obtained by legal means and that any licenses required for their use have been properly purchased and kept up to date. The artist further certifies that any stock images or data which are used to create the deliverables has been properly licensed and is legal to use within the legal framework to which they are subjected. The artist further indemnifies Artconomy and the Commissioner from any and all losses related to errors or misrepresentation in this licensing.
         </p>
         <p>
-          If no other terms are specified in the order description, the intellectual property rights of the deliverables will be assigned to the commissioner, and the commissioner hereby grants a universal, worldwide, non-revokable license to the artist to transmit, store, transcode, display, and resell the resulting work and any derivitives.
+          If no other terms are specified in the order description, the intellectual property rights of the deliverables will be assigned to the commissioner, and the commissioner hereby grants a universal, worldwide, non-revocable license to the artist to transmit, store, transcode, display, and resell the resulting work and any derivatives.
         </p>
         <p>
           If the commissioner has specified via the standard order form that the commission is a private commission, the artist will NOT be granted any license to use the resulting work unless explicitly authorized by the commissioner, other than what is required to deliver it to the commissioner.
         </p>
         <p>
-          The commissioner and artist grant any applicable worldwide, universal, non-revokable license required for Artconomy to store, transmit, and transcode the resulting work in order to fulfill its functions as an intermediary and provide portfolio services to the commissioner and artist. Artconomy may not sell or use in advertising the resulting deliverables without explicit written permission from applicable rightsholders.
+          The commissioner and artist grant any applicable worldwide, universal, non-revocable license required for Artconomy to store, transmit, and transcode the resulting work in order to fulfill its functions as an intermediary and provide portfolio services to the commissioner and artist. Artconomy may not sell or use in advertising the resulting deliverables without explicit written permission from applicable rightsholders.
+        </p>
+        <h2>Arbitration</h2>
+        <p>
+          Any controversy or claim arising out of or relating to this contract, or the breach thereof, shall be settled by arbitration administered by the American Arbitration Association under its Commercial Arbitration Rules. The number of arbitrators shall be one. The place of arbitration shall be Houston, Texas. Texas law shall apply. Judgment on the award rendered by the arbitrator(s) may be entered in any court having jurisdiction thereof. The demand for arbitration shall be made within a reasonable time after the claim, dispute or other matter in question has arisen, and in no event shall it be made after two years from when the aggrieved party knew or should have known of the controversy, claim, dispute or breach.
+        </p>
+        <h2>Governing Law</h2>
+        <p>
+          These Terms shall be governed and construed in accordance with the laws of Texas, United States, without regard to its conflict of law provisions.
         </p>
         <p>
-          Artconomy shall at no time be considered liable for copyright or other content, legal or illegal, of the deliverables. Artconomy will make no determination as to the legal status or license of any deliverable or its contents. These liabilities are assumed by the commissioner and artist as determined by any arbitrator or court of law, bearing in mind the other assumed claims of liability in this section.
+          Artconomy's failure to enforce any right or provision of this Agreement will not be considered a waiver of those rights. If any provision of this Agreement is held to be invalid or unenforceable by a court, the remaining provisions of this Agreement will remain in effect. This Agreement constitutes the entire agreement between parties regarding this Commission, and supersedes and replaces any prior agreements that might have between the parties regarding this Commission.
         </p>
       </div>
     </div>
