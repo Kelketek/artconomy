f15c276c8ffbce869b5cd6ab5e5f5f29e8e60276
Fox Piacenti <fox@vulpinity.com>
2018-09-12 13:10:33 -0500

    Updated Character Transfer Agreement

--- frontend/src/components/CharacterTransferAgreement.vue
+++ frontend/src/components/CharacterTransferAgreement.vue
@@ -6,60 +6,80 @@
           <img class="legal-logo" src="/static/images/logo.svg"/>
         </div>
         <div class="col-12 text-xs-center home-title">
-          <h1>Character Transfer Agreement</h1>
+          <h1>Character Transfer Agreement (Assignment of Copyright)</h1>
         </div>
       </div>
       <div class="col-12-sm">
-        <p><strong>Last updated: July 03, 2018</strong></p>
+        <p><strong>Last updated: September 12, 2018</strong></p>
         <p>
-          This agreement covers the expectations for characters who are transferred, auctioned, or sold. A character
-          made for transfer will be referred to as an 'adoptable' for the purposes of this agreement. Regardless of
-          of compensation specified, or lack thereof, the transferer will be referred to as the seller for the purpose
-          of this document, and the recipient the buyer. The transaction may be referred to as a transfer or a
-          sale alternately with an understood equivalence.
-        </p>
-        <p>
-          <strong>Artconomy is not aware of any unambiguous intellectual property law concerning individual characters
-          not registered as trademarks. Such laws may exist in the future or may exist in jurisdictions
-          outside of Artconomy's discovery. Parties to this agreement affirm to use it to resolve disputes in
-          any arbitration or court of law that will admit it in lieu of and/or in supplement of statutes or
-          precedence supporting a market of adoptables.</strong>
+          This agreement covers the assignment of copyright for a collection of works ("Portfolio") which contains
+          a written description of a character ("Character") and zero or more artistic renderings of the Character
+          between the current rights holder ("Owner"), the purchaser and assignee ("Transferee"), and the platform for transfer
+          of the Portfolio (Artconomy), collectively known as "The Parties". The price and included works within the
+          Portfolio are cataloged on the transfer page from which this agreement is referenced ("Exhibit A"). If the
+          price is listed as $0.00 on Exhibit A, an alternative price may have been agreed to by the Owner and the
+          Transferee, evidence for which will be provided by The Parties in any dispute arising from this agreement.
         </p>
         <h2>Affirmation of Ownership</h2>
         <p>
-          The seller of the adoptable in this transfer affirms that they have created it and retained all rights or have
-          otherwise obtained full rights, whatever they may entail, including right of transferrence of the adoptable.
+          The Owner warrants that he/she is the sole creator and/or owner of all copyrights in the
+          Portfolio, and that he/she has full power to enter into this Agreement and that this Agreement does not infringe
+          the rights of any third party.
         </p>
-        <h2>Terms of Transfer</h2>
+        <h2>Terms of Assignment</h2>
         <p>
-          The seller of this adoptable agrees to relinquish any and all rights, express or implied to this adoptable,
-          unless otherwise exempted by the buyer in writing. These include but are not limited to the right to claim the
-          adoptable as the seller's 'own', the right to create new works including or derived from the adoptable,
-          trademarks, and the right to sell or otherwise transfer rights to the adoptable in any future agreements.
+          The Owner agrees that the Assignee shall from here on own the said copyrights in the Portfolio, to benefit and
+          dispose of these rights in any way and at his/her sole discretion, including all rights as they pertain to
+          the Character and portrayed within the Portfolio, such as the right to create derivative works using
+          the Character.
         </p>
-        <p>The seller affirms that the rights of any works included with this transfer (see section: Included Works)
-          are currently held by them, and any qualifiers to these rights will be communicated ahead of sale. The seller
-          assumes liability for any pieces or the adoptable misrepresented, willful or otherwise if the buyer comes to
-          legal harm from a third party with a claim to the adoptable or included works as a result of this
-          misrepresentation.
+        <p>The Transferee agrees to furnish payment in the amount indicated on Exhibit A in exchange for the rights, or
+          an amount previously agreed to in writing by both parties if the amount indicated on Exhibit A is $0.00.
         </p>
-        <h3>Included Works</h3>
         <p>
-          The seller may elect to transfer the rights to all submissions currently on file with Artconomy which the
-          seller is the controlling user and which the adoptable is featured in. This transfer shall be considered full
-          and absolute, with no rights reserved, even if other characters are depicted in the pieces. However, no
-          additional rights will be transferred concerning characters which are not involving the included adoptable,
-          and all previously licensed rights to the pieces given to third parties (such as right of reproduction) before
-          this transfer will be unaffected by this sale, especially those granted to Artconomy.
+          No additional rights will be transferred concerning characters which are not the Character but which may also
+          be depicted in the Portfolio, and all previously licensed rights to the Portfolio's contents granted to
+          Artconomy will remain, especially those granted for its service in portfolio management.
         </p>
-        <p>Included works will only be understood to include assets held on Artconomy's </p>
+        <p>The Parties agree that this Agreement shall be binding upon the legal successors and assigns.</p>
+        <p>The Owner agrees to sign all necessary papers to procure registration of, or as further necessary to effect
+          the assignment under this Agreement, as and where necessary.</p>
+        <p>The parties agree to pay fees to Artconomy on the amount transferred.
+          The transfer of rights will be final, with no refunds given. If any dispute arises concerning the amount
+          transferred, The Parties agree that Artconomy will still be entitled to its fees collected upon the initial
+          transfer.
+        </p>
+        <p>The price may be listed as $0.00 on Exhibit A, in which case no fees will be collected but all other terms
+          of this agreement will hold.
+        </p>
         <h2>Limitation of Liability/Warranty Waiver</h2>
         <p>
-          The adoptable is made without Warranty of suitability for any purpose, express or implied.
+          The Portfolio's rights are assigned without Warranty of suitability for any purpose, express or implied.
         </p>
-        <h3>Idemnity</h3>
+        <h3>Indemnity</h3>
         <p>Artconomy shall not be held liable for any misrepresentation or error on the part of the seller, and has
-          performed no discovery on the rights of the adoptable and included works in this sale.</p>
+          performed no discovery on the rights regarding the Portfolio and the Character.
+          The Transferee is responsible for any discovery and verification required.</p>
+        <h2>Arbitration</h2>
+        <p>
+          Any controversy or claim arising out of or relating to this contract,
+          or the breach thereof, shall be settled by arbitration administered
+          by the American Arbitration Association under its Commercial Arbitration Rules.
+          The number of arbitrators shall be one.  The place of arbitration shall be Houston, Texas.
+          Texas law shall apply.  Judgment on the award rendered by the
+          arbitrator(s) may be entered in any court having jurisdiction
+          thereof. The
+          demand for arbitration shall be made within a reasonable time after the
+          claim, dispute or other
+          matter in question has arisen, and in no event shall it
+          be made after two years from when the aggrieved party knew or should have
+          known of the controversy, claim, dispute or breach.
+        </p>
+        <p>
+          Artconomy's failure to enforce any right or provision of this Agreement will not be considered a waiver of
+          those rights. If any provision of this Agreement is held to be invalid or unenforceable by a court, the
+          remaining provisions of this Agreement will remain in effect.
+        </p>
       </div>
     </div>
   </div>
