c4fdb71debc9f7b9b014bf7e58858bd94c4bc627
Fox Piacenti <fox@vulpinity.com>
2018-09-19 08:01:03 -0500

    Update character transfer agreement, slight change to terms of service.

--- frontend/src/components/CharacterTransferAgreement.vue
+++ frontend/src/components/CharacterTransferAgreement.vue
@@ -10,56 +10,40 @@
         </div>
       </div>
       <div class="col-12-sm">
-        <p><strong>Last updated: September 12, 2018</strong></p>
+        <p><strong>Last updated: September 19, 2018</strong></p>
         <p>
-          This agreement covers the assignment of copyright for a collection of works ("Portfolio") which contains
+          This agreement covers the assignment of copyright for a collection of works ("Works") which contains
           a written description of a character ("Character") and zero or more artistic renderings of the Character
-          between the current rights holder ("Owner"), the purchaser and assignee ("Transferee"), and the platform for transfer
-          of the Portfolio (Artconomy), collectively known as "The Parties". The price and included works within the
-          Portfolio are cataloged on the transfer page from which this agreement is referenced ("Exhibit A"). If the
-          price is listed as $0.00 on Exhibit A, an alternative price may have been agreed to by the Owner and the
-          Transferee, evidence for which will be provided by The Parties in any dispute arising from this agreement.
+          between the current rights holder ("Author"), the purchaser and Assignee ("Assignee"), collectively known as
+          "The Parties". The price and included works within the Works are cataloged on the transfer page from which
+          this agreement is referenced ("Exhibit A").
         </p>
-        <h2>Affirmation of Ownership</h2>
+        <h2>Affirmation of Authorship</h2>
         <p>
-          The Owner warrants that he/she is the sole creator and/or owner of all copyrights in the
-          Portfolio, and that he/she has full power to enter into this Agreement and that this Agreement does not infringe
-          the rights of any third party.
+          The Author warrants that he/she is the sole creator and/or owner of all copyrights in the
+          Works.
         </p>
         <h2>Terms of Assignment</h2>
-        <p>
-          The Owner agrees that the Assignee shall from here on own the said copyrights in the Portfolio, to benefit and
-          dispose of these rights in any way and at his/her sole discretion, including all rights as they pertain to
-          the Character and portrayed within the Portfolio, such as the right to create derivative works using
-          the Character.
-        </p>
-        <p>The Transferee agrees to furnish payment in the amount indicated on Exhibit A in exchange for the rights, or
-          an amount previously agreed to in writing by both parties if the amount indicated on Exhibit A is $0.00.
-        </p>
-        <p>
-          No additional rights will be transferred concerning characters which are not the Character but which may also
-          be depicted in the Portfolio, and all previously licensed rights to the Portfolio's contents granted to
-          Artconomy will remain, especially those granted for its service in portfolio management.
-        </p>
-        <p>The Parties agree that this Agreement shall be binding upon the legal successors and assigns.</p>
-        <p>The Owner agrees to sign all necessary papers to procure registration of, or as further necessary to effect
-          the assignment under this Agreement, as and where necessary.</p>
-        <p>The parties agree to pay fees to Artconomy on the amount transferred.
-          The transfer of rights will be final, with no refunds given. If any dispute arises concerning the amount
-          transferred, The Parties agree that Artconomy will still be entitled to its fees collected upon the initial
-          transfer.
-        </p>
-        <p>The price may be listed as $0.00 on Exhibit A, in which case no fees will be collected but all other terms
-          of this agreement will hold.
+        <p>Author does hereby sell, assign, transfer, and convey unto Assignee, the entire right, title, and interest in and to said Works and in and to any and all copyrights on said Works that may arise and/or may be granted in the United States and any foreign country, and including each and every derivative WORK arising from said Works.</p>
+
+        <p>Author does hereby covenant and agree to cooperate with Assignee whereby Assignee may enjoy to the fullest extent the right, title, and interest herein conveyed.  Such cooperation shall include:</p>
+
+        <p>prompt execution of all papers (prepared at the expense of Assignee) which are deemed necessary or desirable by Assignee to perfect in it the right, title, and interest herein conveyed;</p>
+
+        <p>prompt execution of all petitions, oaths, specifications, declarations, or other papers (prepared at the expense of Assignee) which are deemed necessary or desirable by Assignee for obtaining copyright registration in the United States and/or foreign countries covering said Works or works derived from said Works; and</p>
+
+        <p>prompt assistance and cooperation in the prosecution of legal proceedings involving said Works or derivative works therefrom, said registrations granted thereon, including proceedings before any Copyright Office of the United States or any foreign country, and court actions, provided, however, that the expense which may be incurred by said Author in lending such assistance and cooperation be paid by Assignee.</p>
+
+        <p>The terms, covenants, and provisions of this assignment shall inure to the benefit of Assignee, its successors, assigns, and/or other legal representatives, and shall be binding upon said Author, his respective heirs, legal representatives, and assigns.</p>
+
+        <p>Author warrants and represents that he has not entered into any assignment, contract, or understanding in conflict herewith.</p>
+
+        <p>The Assignee will furnish payment in the amount indicated on Exhibit A in exchange for the rights.
         </p>
         <h2>Limitation of Liability/Warranty Waiver</h2>
         <p>
-          The Portfolio's rights are assigned without Warranty of suitability for any purpose, express or implied.
+          The Works's rights are assigned without Warranty of suitability for any purpose, express or implied.
         </p>
-        <h3>Indemnity</h3>
-        <p>Artconomy shall not be held liable for any misrepresentation or error on the part of the seller, and has
-          performed no discovery on the rights regarding the Portfolio and the Character.
-          The Transferee is responsible for any discovery and verification required.</p>
         <h2>Arbitration</h2>
         <p>
           Any controversy or claim arising out of or relating to this contract,
@@ -74,11 +58,6 @@
           matter in question has arisen, and in no event shall it
           be made after two years from when the aggrieved party knew or should have
           known of the controversy, claim, dispute or breach.
-        </p>
-        <p>
-          Artconomy's failure to enforce any right or provision of this Agreement will not be considered a waiver of
-          those rights. If any provision of this Agreement is held to be invalid or unenforceable by a court, the
-          remaining provisions of this Agreement will remain in effect.
         </p>
       </div>
     </div>
