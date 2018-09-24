e5e91ffc3aee48e1cfbfc29a42dbb97d6c7f243c
Fox Piacenti <fox@vulpinity.com>
2018-09-24 16:37:10 -0500

    Make observe visibility check work in Safari via Polyfill
    Signed-off-by: Fox Piacenti <jonathan@vulpinity.com>

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'intersection-observer'
 import VueFormGenerator from 'vue-form-generator'
 import 'vuetify/dist/vuetify.min.css'
 import './artconomy.css'
