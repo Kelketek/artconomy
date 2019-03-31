07ab8758620b19c4ff8de1a48a8294709b2ed275
Fox Piacenti <fox@vulpinity.com>
2019-03-31 18:06:42 -0500

    Remove TFF banner.

--- backend/apps/profiles/management/commands/demo_data.py
+++ backend/apps/profiles/management/commands/demo_data.py
@@ -1,4 +1,6 @@
 from django.core.management.base import BaseCommand
+
+from apps.lib.abstract_models import MATURE
 from apps.profiles.models import User
 from apps.profiles.tests.factories import UserFactory
 from django.conf import settings
@@ -12,9 +14,9 @@ class Command(BaseCommand):
             raise RuntimeError("You can't pull this shit in production, man!")
         UserFactory.create(
             username='Fox', bank_account_status=User.HAS_US_ACCOUNT, email='fox@artconomy.com',
-            password='muffinmuffin', is_superuser=True, is_staff=True
+            password='muffinmuffin', is_superuser=True, is_staff=True, rating=MATURE,
         )
         UserFactory.create(
             username='Vulpes', bank_account_status=User.HAS_US_ACCOUNT, email='vulpes@vulpinity.com',
-            password='muffinmuffin'
+            password='muffinmuffin', rating=MATURE,
         )
