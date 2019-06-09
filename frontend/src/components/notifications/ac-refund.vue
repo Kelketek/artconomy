9c4c15edaa291099a7da5812c1ac0459e5edd7f2
Fox Piacenti <fox@vulpinity.com>
2019-06-09 15:27:12 -0500

    Fix display of refund notification.

--- frontend/src/components/notifications/ac-refund.vue
+++ frontend/src/components/notifications/ac-refund.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="col-4">
-    <ac-asset :terse="true" :asset="event.target"></ac-asset>
-  </div>
+  <ac-base-notification :notification="notification" :asset-link="assetLink">
+    <span slot="title"><router-link :to="assetLink">Sale #{{event.target.id}} was refunded. :(</router-link></span>
+  </ac-base-notification>
 </template>
 
 <script>
-  import AcAsset from '../ac-asset'
   import Notification from '../../mixins/notification'
+  import AcBaseNotification from './ac-base-notification'
+
   export default {
     name: 'ac-refund',
     mixins: [Notification],
-    components: {AcAsset},
+    components: {AcBaseNotification},
     data () {
       return {}
-    }
+    },
+    computed: {
+      assetLink () {
+        return {
+          name: 'Sale',
+          params: {
+            username: this.event.target.seller.username,
+            saleID: this.event.target.id,
+          },
+        }
+      },
+    },
   }
 </script>
