d67ea7e1abb6b66463c2c4e48ccab0a3ef331283
Fox Danger Piacenti <fox@artconomy.com>
2019-08-15 12:45:11 -0500

    Don't show add on profile page for products, since it's broken.

--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -63,7 +63,7 @@
     </v-tabs>
     <v-tabs-items v-model="tab" class="min-height">
       <v-tab-item value="tab-products" :class="{'tab-shown': shownTab('tab-products')}">
-        <store :endpoint="`/api/sales/v1/account/${username}/products/`" :username="username" :track-pages="true" tab-name="tab-products"/>
+        <store :endpoint="`/api/sales/v1/account/${username}/products/`" :embedded="true" :username="username" :track-pages="true" tab-name="tab-products"/>
       </v-tab-item>
       <v-tab-item value="tab-characters" :class="{'tab-shown': shownTab('tab-characters')}">
         <Characters
