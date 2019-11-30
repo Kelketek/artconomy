2215c4dd7ca853142ef621279b061d1b85108543
Fox Danger Piacenti <fox@artconomy.com>
2019-11-30 16:23:43 -0600

    Fix landing page mixin to not load full nav for AlwaysOpen.

--- frontend/mixins/nav.ts
+++ frontend/mixins/nav.ts
@@ -4,6 +4,6 @@ import Component from 'vue-class-component'
 @Component
 export default class Nav extends Vue {
   public get fullInterface() {
-    return ['NewOrder', 'Shield'].indexOf(this.$route.name as string) === -1
+    return ['NewOrder', 'Shield', 'AlwaysOpen'].indexOf(this.$route.name as string) === -1
   }
 }
