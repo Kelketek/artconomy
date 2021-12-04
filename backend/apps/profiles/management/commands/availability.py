128b26f5b8abfa6ab3fd896a5ac1220aa6be5d5e
Fox Danger Piacenti <fox@artconomy.com>
2021-12-04 15:14:50 -0600

    Update availability management command.

--- backend/apps/profiles/management/commands/availability.py
+++ backend/apps/profiles/management/commands/availability.py
@@ -1,9 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.db import transaction
 
-from apps.lib.utils import require_lock
-from apps.profiles.models import User
-from apps.sales.models import Order, Product
+from apps.profiles.models import ArtistProfile
 from apps.sales.utils import update_availability
 
 
@@ -11,12 +9,9 @@ class Command(BaseCommand):
     help = 'Runs update availability on all users.'
 
     @transaction.atomic
-    @require_lock(User, 'ACCESS EXCLUSIVE')
-    @require_lock(Order, 'ACCESS EXCLUSIVE')
-    @require_lock(Product, 'ACCESS EXCLUSIVE')
-    def run_update(self, user):
-        update_availability(user, user.load, user.commissions_disabled)
+    def run_update(self, artist_profile):
+        update_availability(artist_profile.user, artist_profile.load, artist_profile.commissions_disabled)
 
     def handle(self, *args, **options):
-        for user in User.objects.all():
-            self.run_update(user)
+        for artist_profile in ArtistProfile.objects.all():
+            self.run_update(artist_profile)
