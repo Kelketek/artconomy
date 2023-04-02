e7bbf0dbbea198926409b78a315093aa9e3cb434
Fox Danger Piacenti <fox@opencraft.com>
2023-04-01 20:46:39 -0400

    fix: Typo in ensure_invoice_annotations

--- backend/apps/sales/management/commands/ensure_invoice_annotations.py
+++ backend/apps/sales/management/commands/ensure_invoice_annotations.py
@@ -9,5 +9,5 @@ class Command(BaseCommand):
         for deliverable in Deliverable.objects.exclude(invoice__isnull=True):
             deliverable_ref = ref_for_instance(deliverable)
             invoice_ref = ref_for_instance(deliverable.invoice)
-            for transaction in TransactionRecord.object.filter(targets=deliverable_ref):
+            for transaction in TransactionRecord.objects.filter(targets=deliverable_ref):
                 transaction.targets.add(invoice_ref)
