c03e7ba4af7cb55a7ea9f8236c92bf57f7f8f074
Fox Danger Piacenti <fox@opencraft.com>
2023-12-07 05:00:20 -0600

    fix: Cloudflare IP logging.

--- ansible/roles/nginx/files/cloudflare_ips.py
+++ ansible/roles/nginx/files/cloudflare_ips.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 from __future__ import print_function
 
 import hashlib
@@ -9,34 +9,34 @@ from sys import exit, stderr
 import requests
 
 try:
-    response_ip4 = requests.get('https://www.cloudflare.com/ips-v4')
+    response_ip4 = requests.get("https://www.cloudflare.com/ips-v4")
     response_ip4.raise_for_status()
 except Exception as err:
-    print('Could not reach IP4 list: {}'.format(err), file=stderr)
+    print("Could not reach IP4 list: {}".format(err), file=stderr)
     exit(1)
 
 try:
-    response_ip6 = requests.get('https://www.cloudflare.com/ips-v6')
+    response_ip6 = requests.get("https://www.cloudflare.com/ips-v6")
     response_ip6.raise_for_status()
 except Exception as err:
-    print('Could not reach IP6 list: {}'.format(err), file=stderr)
+    print("Could not reach IP6 list: {}".format(err), file=stderr)
     exit(2)
 
-result = ''
-for line in chain(response_ip4.content.split('\n'), response_ip6.content.split('\n')):
+result = ""
+for line in chain(response_ip4.content.split("\n"), response_ip6.content.split("\n")):
     line = line.strip()
     if not line:
         continue
-    result += 'set_real_ip_from {};\n'.format(line)
-result += 'real_ip_header CF-Connecting-IP;\n'
+    result += "set_real_ip_from {};\n".format(line)
+result += "real_ip_header CF-Connecting-IP;\n"
 new_hasher = hashlib.md5()
 new_hasher.update(result)
 old_hasher = hashlib.md5()
-with open('/etc/nginx/includes/real_ip.conf', 'r') as f:
+with open("/etc/nginx/includes/real_ip.conf", "r") as f:
     old_hasher.update(f.read())
 if new_hasher.hexdigest() == old_hasher.hexdigest():
     exit(0)
-with open('/etc/nginx/includes/real_ip.conf', 'w') as f:
+with open("/etc/nginx/includes/real_ip.conf", "w") as f:
     f.write(result)
     f.flush()
-os.system('service nginx restart >/dev/null')
+os.system("service nginx reload >/dev/null")
