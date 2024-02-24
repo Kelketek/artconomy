aa4b12cb9911dc277b4c4e7776dd03c548ddc6ce
Fox Danger Piacenti <fox@opencraft.com>
2024-02-24 16:04:38 -0600

    refactor: Convert Home and App components to compositional.

--- frontend/plugins/targets.ts
+++ frontend/plugins/targets.ts
@@ -3,7 +3,7 @@
 // For floating items, we specify the 'attach' property and depending on which way this plugin is configured it will be
 // undefined or else it will be the name of a div that's only really needed in a test environment.
 
-import {createApp} from 'vue'
+import {computed, createApp, inject} from 'vue'
 
 export const createTargetsPlugin = (testMode: boolean) => {
   const plugin =  {
@@ -25,6 +25,17 @@ export const createTargetsPlugin = (testMode: boolean) => {
   return {
     install(app: ReturnType<typeof createApp>) {
       app.mixin(plugin)
+      app.provide('testMode', testMode)
     }
+  }
+}
+
+export const useTargets = () => {
+  const testMode = inject('testMode')
+  return {
+    modalTarget: computed(() =>  testMode ? '#modal-target' : false),
+    snackbarTarget: computed(() => testMode ? '#snackbar-target': false),
+    statusTarget: computed(() => testMode ? '#status-target': false),
+    menuTarget: computed(() => testMode ? '#menu-target': false),
   }
 }
