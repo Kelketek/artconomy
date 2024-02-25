3c8adea8fd0e568afb5d2fb6667d60e92a20f4ad
Fox Danger Piacenti <fox@opencraft.com>
2024-02-24 18:07:16 -0600

    refactor: Bound field to compositional.

--- frontend/components/fields/AcBoundField.ts
+++ frontend/components/fields/AcBoundField.ts
@@ -1,5 +1,4 @@
-import {h, resolveComponent, defineAsyncComponent} from 'vue'
-import {Component, Prop, toNative, Vue} from 'vue-facing-decorator'
+import {h, resolveComponent, defineAsyncComponent, defineComponent, useSlots, useAttrs, computed} from 'vue'
 import {FieldController} from '@/store/forms/field-controller.ts'
 const AcUserSelect = defineAsyncComponent(() => import('@/components/fields/AcUserSelect.vue'))
 const AcEditor = defineAsyncComponent(() => import('@/components/fields/AcEditor.vue'))
@@ -17,12 +16,13 @@ import {VTextField} from 'vuetify/lib/components/VTextField/index.mjs'
 import {VAutocomplete} from 'vuetify/lib/components/VAutocomplete/index.mjs'
 import {VSlider} from 'vuetify/lib/components/VSlider/index.mjs'
 import {VSelect} from 'vuetify/lib/components/VSelect/index.mjs'
-import {VRadio} from 'vuetify/lib/components/VRadio/index.mjs'
+import {VRadio as BaseVRadio} from 'vuetify/lib/components/VRadio/index.mjs'
+
+const VRadio = BaseVRadio.default
 
 const canonicalFields = ['input', 'button', 'textarea', 'select']
 
-// Any components that might be used as a field and which aren't in Vuetify must be added here to resolve.
-@Component({
+export default defineComponent({
   components: {
     AcUserSelect,
     AcEditor,
@@ -40,43 +40,47 @@ const canonicalFields = ['input', 'button', 'textarea', 'select']
     VAutocomplete,
     VSlider,
     VSelect,
-    VRadio: VRadio.default,
+    VRadio,
   },
+  props: {
+    fieldType: {
+      default: 'v-text-field',
+      type: String,
+    },
+    fieldId: {
+      required: false,
+      type: String,
+    },
+    field: {
+      type: FieldController,
+      required: true,
+    }
+  },
+  setup: (props) => {
+    const slots = useSlots()
+    const passedAttrs = useAttrs()
+    const attrs = computed(() => {
+      // We don't really know which keys are props and which are attrs. That's for the child component to decide, for the
+      // most part. Here, we try to make our best guess and send almost everything except for things we know Vuetify
+      // will complain about, since all, or nearly all, of our inputs are based on Vuetify components.
+      let base: Record<string, any>
+      if (canonicalFields.indexOf(props.fieldType) === -1) {
+        base = {...props.field.bind}
+      } else {
+        base = {...props.field.rawBind}
+      }
+      if (props.fieldId) {
+        base.id = props.fieldId
+      }
+      return {
+        ...base,
+        ref: 'input', ...passedAttrs,
+      }
+    })
+    return () => {
+      return h(resolveComponent(props.fieldType), {
+        ...attrs,
+      }, slots)
+    }
+  }
 })
-class AcBoundField extends Vue {
-  @Prop({default: 'v-text-field'})
-  public fieldType!: string
-
-  @Prop()
-  public fieldId!: string | undefined
-
-  @Prop({required: true})
-  public field!: FieldController
-
-  public get attrs() {
-    // We don't really know which keys are props and which are attrs. That's for the child component to decide, for the
-    // most part. Here, we try to make our best guess and send almost everything except for things we know Vuetify
-    // will complain about, since all, or nearly all, of our inputs are based on Vuetify components.
-    let base: Record<string, any>
-    if (canonicalFields.indexOf(this.fieldType) === -1) {
-      base = {...this.field.bind}
-    } else {
-      base = {...this.field.rawBind}
-    }
-    if (this.fieldId) {
-      base.id = this.fieldId
-    }
-    return {
-      ...base,
-      ref: 'input', ...this.$attrs,
-    }
-  }
-
-  public render() {
-    return h(resolveComponent(this.fieldType), {
-      ...this.attrs,
-    }, this.$slots)
-  }
-}
-
-export default toNative(AcBoundField)
