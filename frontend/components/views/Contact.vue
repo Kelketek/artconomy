0661688a5f2d18db1b376cff3774311b9699a9cb
Fox Danger Piacenti <fox@opencraft.com>
2024-06-24 11:52:25 -0500

    refactor: Contact to compositional.

--- frontend/components/views/Contact.vue
+++ frontend/components/views/Contact.vue
@@ -4,17 +4,15 @@
       <v-col class="py-5 text-center" cols="12" md="6" offset-md="3">
         <h1>Artconomy</h1>
         <address>
-          1200 N Dairy Ashford Rd<br/>
-          Suite #583<br/>
-          Houston, TX 77079
+          5214 Newkirk Lane<br/>
+          Houston, TX 77021
         </address>
         <a href="tel:+19033263699">1-903-326-FOXY(3699)</a><br/>
-        info@artconomy.com
+        contact@artconomy.com
       </v-col>
     </v-row>
   </v-container>
 </template>
 
-<script lang="ts">
-export default {}
+<script setup lang="ts">
 </script>
