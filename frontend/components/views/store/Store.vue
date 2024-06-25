b2514f8b95f92e04f0899a9f0c638af8c037d642
Fox Danger Piacenti <fox@opencraft.com>
2024-06-25 13:37:17 -0500

    refactor: Store to compositional.

--- frontend/components/views/store/Store.vue
+++ frontend/components/views/store/Store.vue
@@ -2,43 +2,24 @@
   <v-container fluid class="pa-0">
     <router-view v-if="!currentRoute"/>
     <v-container v-else>
-      <ac-profile-header :username="username" v-if="!$store.state.iFrame"/>
+      <ac-profile-header :username="username" v-if="!store.state.iFrame"/>
       <ac-subjective-product-list :username="username"/>
     </v-container>
   </v-container>
 </template>
 
-<script lang="ts">
-import Subjective from '@/mixins/subjective.ts'
-import {Component, mixins, toNative} from 'vue-facing-decorator'
-import AcProductList from '@/components/views/store/AcProductList.vue'
+<script setup lang="ts">
 import AcProfileHeader from '@/components/views/profile/AcProfileHeader.vue'
-import AcFormDialog from '@/components/wrappers/AcFormDialog.vue'
-import AcLoadSection from '@/components/wrappers/AcLoadSection.vue'
-import AcBankToggle from '@/components/fields/AcBankToggle.vue'
-import AcPatchField from '@/components/fields/AcPatchField.vue'
-import AcBoundField from '@/components/fields/AcBoundField.ts'
-import AcNewProduct from '@/components/views/store/AcNewProduct.vue'
 import AcSubjectiveProductList from '@/components/views/store/AcSubjectiveProductList.vue'
+import {computed} from 'vue'
+import {useRoute} from 'vue-router'
+import {useStore} from 'vuex'
+import {ArtState} from '@/store/artState.ts'
+import SubjectiveProps from '@/types/SubjectiveProps.ts'
 
-@Component({
-  components: {
-    AcSubjectiveProductList,
-    AcNewProduct,
-    AcBoundField,
-    AcPatchField,
-    AcBankToggle,
-    AcLoadSection,
-    AcFormDialog,
-    AcProfileHeader,
-    AcProductList,
-  },
-})
-class Store extends mixins(Subjective) {
-  public get currentRoute() {
-    return ['Store', 'StoreiFrame', 'ManageStore'].indexOf(String(this.$route!.name) + '') !== -1
-  }
-}
 
-export default toNative(Store)
+const props = defineProps<SubjectiveProps>()
+const store = useStore<ArtState>()
+const route = useRoute()
+const currentRoute = computed(() => ['Store', 'StoreiFrame', 'ManageStore'].indexOf(String(route!.name) + '') !== -1)
 </script>
