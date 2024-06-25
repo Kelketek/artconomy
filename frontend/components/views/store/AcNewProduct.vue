4ed9983638ec4fb5aec576ef320d4c12b5f6a39f
Fox Danger Piacenti <fox@opencraft.com>
2024-06-25 13:14:48 -0500

    refactor: AcNewProduct to compositional.

--- frontend/components/views/store/AcNewProduct.vue
+++ frontend/components/views/store/AcNewProduct.vue
@@ -165,7 +165,7 @@
                         color="primary"
                     />
                   </v-col>
-                  <v-col cols="12" sm="6" v-if="subject!.paypal_configured">
+                  <v-col cols="12" sm="6" v-if="(subject as User)!.paypal_configured">
                     <ac-bound-field
                         :field="newProduct.fields.paypal"
                         field-type="v-switch"
@@ -274,252 +274,238 @@
     </template>
   </ac-form-dialog>
 </template>
-<script lang="ts">
+<script setup lang="ts">
+import {User} from '@/store/profiles/types/User.ts'
 import AcFormDialog from '@/components/wrappers/AcFormDialog.vue'
 import AcLoadSection from '@/components/wrappers/AcLoadSection.vue'
 import AcPatchField from '@/components/fields/AcPatchField.vue'
 import AcBoundField from '@/components/fields/AcBoundField.ts'
-import Subjective from '@/mixins/subjective.ts'
-import {Component, Prop, toNative, Watch} from 'vue-facing-decorator'
-import AcPricePreview from '@/components/price_preview/AcPricePreview.vue'
-import {FormController} from '@/store/forms/form-controller.ts'
+import {useSubject} from '@/mixins/subjective.ts'
 import Product from '@/types/Product.ts'
-import {SingleController} from '@/store/singles/controller.ts'
 import Pricing from '@/types/Pricing.ts'
 import {flatten} from '@/lib/lib.ts'
 import {Ratings} from '@/types/Ratings.ts'
 import {deliverableLines} from '@/lib/lineItemFunctions.ts'
 import AcPriceComparison from '@/components/price_preview/AcPriceComparison.vue'
-import {LineItemSetMap} from '@/types/LineItemSetMap.ts'
 import {RawLineItemSetMap} from '@/types/RawLineItemSetMap.ts'
 import LineItem from '@/types/LineItem.ts'
+import SubjectiveProps from '@/types/SubjectiveProps.ts'
+import {useErrorHandling} from '@/mixins/ErrorHandling.ts'
+import {useSingle} from '@/store/singles/hooks.ts'
+import {useForm} from '@/store/forms/hooks.ts'
+import {computed, watch} from 'vue'
+import {useRouter} from 'vue-router'
+import {useList} from '@/store/lists/hooks.ts'
+import {ListController} from '@/store/lists/controller.ts'
 
-@Component({
-  components: {
-    AcPriceComparison,
-    AcPricePreview,
-    AcBoundField,
-    AcPatchField,
-    AcLoadSection,
-    AcFormDialog,
+const props = defineProps<{modelValue: boolean} & SubjectiveProps>()
+const emit = defineEmits<{'update:modelValue': [boolean]}>()
+const router = useRouter()
+const {subjectHandler, subject} = useSubject(props)
+const {setError} = useErrorHandling()
+
+subjectHandler.artistProfile.get().catch(setError)
+const pricing = useSingle<Pricing>('pricing', {endpoint: '/api/sales/pricing-info/'})
+pricing.get()
+
+const url = computed(() => `/api/sales/account/${props.username}/products/`)
+const newProduct = useForm(`${flatten(props.username)}__newProduct`, {
+  endpoint: url.value,
+  fields: {
+    name: {value: ''},
+    description: {value: ''},
+    details_template: {value: ''},
+    base_price: {
+      value: '25.00',
+      step: 2,
+      validators: [{name: 'numeric'}],
+    },
+    expected_turnaround: {
+      value: 5,
+      step: 2,
+      validators: [{name: 'numeric'}],
+    },
+    max_rating: {
+      value: Ratings.GENERAL,
+      step: 2,
+    },
+    wait_list: {value: false},
+    task_weight: {
+      value: 1,
+      step: 3,
+    },
+    revisions: {
+      value: 1,
+      step: 2,
+    },
+    max_parallel: {
+      value: 0,
+      step: 3,
+    },
+    hidden: {value: false},
+    table_product: {value: false},
+    tags: {value: []},
+    cascade_fees: {
+      value: false,
+      step: 2,
+    },
+    escrow_enabled: {
+      value: true,
+      step: 2,
+    },
+    escrow_upgradable: {
+      value: false,
+      step: 2,
+    },
+    paypal: {
+      value: true,
+      step: 2,
+    },
+    name_your_price: {
+      value: false,
+      step: 2,
+    },
   },
-  emits: ['update:modelValue'],
 })
-class AcNewProduct extends Subjective {
-  public pricing = null as unknown as SingleController<Pricing>
-  @Prop({required: true})
-  public modelValue!: boolean
 
-  public newProduct = null as unknown as FormController
+const escrow = computed(() => {
+  const profile = subjectHandler.artistProfile.x
+  return profile && profile.bank_account_status === 1
+})
 
-  public get url() {
-    return `/api/sales/account/${this.username}/products/`
-  }
+const goToProduct = (product: Product) => {
+  router.push(
+      {
+        name: 'Product',
+        params: {
+          username: props.username,
+          productId: product.id + '',
+        },
+        query: {editing: 'true'},
+      },
+  )
+}
 
-  public get limitAtOnce() {
-    return this.newProduct.fields.max_parallel.value !== 0
-  }
-
-  @Watch('rawLineItemSetMaps')
-  public updateListControllers(rawLineItemSetMaps: RawLineItemSetMap[]) {
-    for (const set of rawLineItemSetMaps) {
-      const controller = this.$getList(`newProduct${set.name}`, {endpoint: '#', paginated: false})
-      controller.makeReady(set.lineItems)
-    }
-  }
-
-  public get lineItemSetMaps(): LineItemSetMap[] {
-    const sets = []
-    for (const set of this.rawLineItemSetMaps) {
-      const controller = this.$getList(`newProduct${set.name}`, {endpoint: '#', paginated: false})
-      sets.push({name: set.name, lineItems: controller, offer: set.offer})
-    }
-    return sets
-  }
-
-  public get rawLineItemSetMaps(): RawLineItemSetMap[] {
-    if (!(this.pricing && this.pricing.x)) {
-      return []
-    }
-    const sets = []
-    const pricing = this.pricing.x
-    const basePrice = parseFloat(this.newProduct.fields.base_price.value)
-    // eslint-disable-next-line camelcase
-    const planName = this.subject?.service_plan
-    const international = !!this.subject?.international
-    const cascade = this.newProduct.fields.cascade_fees.value
-    const tableProduct = this.newProduct.fields.table_product.value
-    let preferredLines: LineItem[] = []
-    let appendPreferred = false
-    const options = {
-      basePrice,
-      cascade: cascade && (this.newProduct.fields.escrow_enabled.value),
-      international,
-      pricing,
-      escrowEnabled: true,
-      tableProduct,
-      extraLines: [],
-    }
-    if (this.escrow && (this.newProduct.fields.escrow_enabled.value || this.newProduct.fields.escrow_upgradable.value)) {
-      const escrowLines = deliverableLines({
-        ...options,
-        planName,
-      })
-      sets.push({
-        name: 'Shielded',
-        lineItems: escrowLines,
-        offer: false,
-      })
-      if (pricing && (planName !== pricing.preferred_plan)) {
-        preferredLines = deliverableLines({
-          ...options,
-          planName: pricing.preferred_plan,
-        })
-        appendPreferred = true
-      }
-    }
-    if (!this.escrow || !this.newProduct.fields.escrow_enabled.value) {
-      const nonEscrowLines = deliverableLines({
-        basePrice,
-        cascade,
-        international,
-        planName,
-        pricing,
-        escrowEnabled: false,
-        tableProduct,
-        extraLines: [],
-      })
-      sets.push({
-        name: 'Unshielded',
-        lineItems: nonEscrowLines,
-        offer: false,
-      })
-    }
-    if (appendPreferred) {
-      // eslint-disable-next-line camelcase
-      sets.push({
-        name: pricing?.preferred_plan + '',
-        lineItems: preferredLines,
-        offer: true,
-      })
-    }
-    return sets
-  }
-
-  public set limitAtOnce(val: boolean) {
-    const field = this.newProduct.fields.max_parallel
-    if (val) {
+const limitAtOnce = computed({
+  get: () => newProduct.fields.max_parallel.value !== 0,
+  set: (value: boolean) => {
+    const field = newProduct.fields.max_parallel
+    if (value) {
       field.update(field.value || 1)
     } else {
       field.update(0)
     }
   }
+})
 
-  public get basePriceLabel() {
-    if (this.newProduct.fields.cascade_fees.model) {
-      return 'List Price'
-    } else {
-      return 'Take home amount'
+const listControllerMaps = new Map(['Shielded', 'Unshielded', '__preferred'].map((name) => [name, useList<LineItem>(`newProduct${name}`, {endpoint: '#', paginated: false})]))
+
+const rawLineItemSetMaps = computed((): RawLineItemSetMap[] => {
+  if (!(pricing.x)) {
+    return []
+  }
+  const sets = []
+  const basePrice = parseFloat(newProduct.fields.base_price.value)
+  // eslint-disable-next-line camelcase
+  const planName = subject.value?.service_plan
+  const international = !!subject.value?.international
+  const cascade = newProduct.fields.cascade_fees.value
+  const tableProduct = newProduct.fields.table_product.value
+  let preferredLines: LineItem[] = []
+  let appendPreferred = false
+  const options = {
+    basePrice,
+    cascade: cascade && (newProduct.fields.escrow_enabled.value),
+    international,
+    pricing: pricing.x,
+    escrowEnabled: true,
+    tableProduct,
+    extraLines: [],
+  }
+  if (escrow.value && (newProduct.fields.escrow_enabled.value || newProduct.fields.escrow_upgradable.value)) {
+    const escrowLines = deliverableLines({
+      ...options,
+      planName,
+    })
+    sets.push({
+      name: 'Shielded',
+      lineItems: escrowLines,
+      offer: false,
+    })
+    if (pricing && (planName !== pricing.x.preferred_plan)) {
+      preferredLines = deliverableLines({
+        ...options,
+        planName: pricing.x.preferred_plan,
+      })
+      appendPreferred = true
     }
   }
-
-  public get escrow() {
-    const profile = this.subjectHandler.artistProfile.x
-    return profile && profile.bank_account_status === 1
+  if (!escrow.value || !newProduct.fields.escrow_enabled.value) {
+    const nonEscrowLines = deliverableLines({
+      basePrice,
+      cascade,
+      international,
+      planName,
+      pricing: pricing.x,
+      escrowEnabled: false,
+      tableProduct,
+      extraLines: [],
+    })
+    sets.push({
+      name: 'Unshielded',
+      lineItems: nonEscrowLines,
+      offer: false,
+    })
   }
+  if (appendPreferred) {
+    // eslint-disable-next-line camelcase
+    sets.push({
+      name: '__preferred',
+      lineItems: preferredLines,
+      offer: true,
+    })
+  }
+  return sets
+})
 
-  public get priceHint() {
-    if (this.escrow) {
-      return `Enter the listing price you want to present to the user. We will calculate what
+const lineItemSetMaps = computed(() => {
+  const sets = []
+  for (const set of rawLineItemSetMaps.value) {
+    const name = listControllerMaps.has(set.name) ? set.name : '__preferred'
+    const controller = listControllerMaps.get(name)
+    sets.push({name: set.name, lineItems: controller, offer: set.offer})
+  }
+  return sets
+})
+
+watch(rawLineItemSetMaps, (rawLineItemSetMaps: RawLineItemSetMap[]) => {
+  for (const set of rawLineItemSetMaps) {
+    let name = listControllerMaps.has(set.name) ? set.name : '__preferred'
+    const controller = listControllerMaps.get(name) as ListController<LineItem>
+    controller.makeReady(set.lineItems)
+  }
+}, {deep: true, immediate: true})
+
+const basePriceLabel = computed(() => {
+  if (newProduct.fields.cascade_fees.model) {
+    return 'List Price'
+  } else {
+    return 'Take home amount'
+  }
+})
+
+const toggle = (value: boolean) => {
+  emit('update:modelValue', value)
+}
+
+const priceHint = computed(() => {
+  if (escrow.value) {
+    return `Enter the listing price you want to present to the user. We will calculate what
                 their fees will be for you. Adjust this number until you're happy with your cut and
                 the total price. You will be able to adjust this
                 price per-order if the client has special requests.`
-    }
-    return `Enter the listing price you want to present to the user. You will be able to adjust this
+  }
+  return `Enter the listing price you want to present to the user. You will be able to adjust this
               price per-order if the client has special requests.`
-  }
-
-  public toggle(value: boolean) {
-    this.$emit('update:modelValue', value)
-  }
-
-  public goToProduct(product: Product) {
-    this.$router.push(
-        {
-          name: 'Product',
-          params: {
-            username: this.username,
-            productId: product.id + '',
-          },
-          query: {editing: 'true'},
-        },
-    )
-  }
-
-  public created() {
-    this.subjectHandler.artistProfile.get().catch(this.setError)
-    this.pricing = this.$getSingle('pricing', {endpoint: '/api/sales/pricing-info/'})
-    this.pricing.get()
-    this.newProduct = this.$getForm(`${flatten(this.username)}__newProduct`, {
-      endpoint: this.url,
-      fields: {
-        name: {value: ''},
-        description: {value: ''},
-        details_template: {value: ''},
-        base_price: {
-          value: '25.00',
-          step: 2,
-          validators: [{name: 'numeric'}],
-        },
-        expected_turnaround: {
-          value: 5,
-          step: 2,
-          validators: [{name: 'numeric'}],
-        },
-        max_rating: {
-          value: Ratings.GENERAL,
-          step: 2,
-        },
-        wait_list: {value: false},
-        task_weight: {
-          value: 1,
-          step: 3,
-        },
-        revisions: {
-          value: 1,
-          step: 2,
-        },
-        max_parallel: {
-          value: 0,
-          step: 3,
-        },
-        hidden: {value: false},
-        table_product: {value: false},
-        tags: {value: []},
-        cascade_fees: {
-          value: false,
-          step: 2,
-        },
-        escrow_enabled: {
-          value: true,
-          step: 2,
-        },
-        escrow_upgradable: {
-          value: false,
-          step: 2,
-        },
-        paypal: {
-          value: true,
-          step: 2,
-        },
-        name_your_price: {
-          value: false,
-          step: 2,
-        },
-      },
-    })
-  }
-}
-
-export default toNative(AcNewProduct)
+})
 </script>
