5cf737afae218e979a5b2ea13c749d5bde4fad7d
Fox Danger Piacenti <fox@opencraft.com>
2024-06-25 19:50:46 -0500

    chore: Remove unused ProductCentric mixin.

--- frontend/components/views/product/mixins/ProductCentric.ts
+++ frontend/components/views/product/mixins/ProductCentric.ts
@@ -1,48 +1,16 @@
-import Subjective from '@/mixins/subjective.ts'
-import {Component, mixins, Prop} from 'vue-facing-decorator'
-import {SingleController} from '@/store/singles/controller.ts'
 import Product from '@/types/Product.ts'
 import {addBusinessDays, formatISO} from 'date-fns'
 import {useSingle} from '@/store/singles/hooks.ts'
-import {computed, watch} from 'vue'
+import {computed} from 'vue'
 import SubjectiveProps from '@/types/SubjectiveProps.ts'
 import ProductProps from '@/types/ProductProps.ts'
 
-@Component
-export default class ProductCentric extends mixins(Subjective) {
-  @Prop({required: true})
-  public productId!: number
-
-  public product: SingleController<Product> = null as unknown as SingleController<Product>
-
-  public get url() {
-    return getUrl(this.username, this.productId)
-  }
-
-  public get deliveryDate() {
-    const product = this.product.x as Product
-    // @ts-ignore
-    return formatISO(addBusinessDays(new Date(), product.expected_turnaround))
-  }
-
-  public created() {
-    this.product = this.$getSingle(`product__${this.productId}`, {endpoint: this.url})
-  }
-}
-
-const getDeliveryDate = (product: Product) => {
-  return formatISO(addBusinessDays(new Date(), product.expected_turnaround))
-}
-
-const getUrl = (username: string, productId: number) => {
-  return `/api/sales/account/${username}/products/${productId}/`
-}
 
 export const useProduct = <T extends SubjectiveProps & ProductProps>(props: T) => {
-  const url = computed(() => getUrl(props.username, parseInt(`${props.productId}`, 10)))
+  const url = computed(() => `/api/sales/account/${props.username}/products/${props.productId}/`)
   const product = useSingle<Product>(`product__${props.productId}`, {endpoint: url.value})
   product.get().then()
-  const deliveryDate = computed(() => product.x && getDeliveryDate(product.x))
+  const deliveryDate = computed(() => product.x && formatISO(addBusinessDays(new Date(), product.x.expected_turnaround)))
   return {
     product,
     deliveryDate,
