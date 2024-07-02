8e58282916b9661f1b00a9728218366aa1d3bee2
Fox Danger Piacenti <fox@opencraft.com>
2024-07-01 23:38:15 -0500

    refactor: AuthViews to compositional.

--- frontend/components/views/auth/AuthViews.vue
+++ frontend/components/views/auth/AuthViews.vue
@@ -3,13 +3,13 @@
     <v-row>
       <v-col cols="12" sm="8" md="6" offset-sm="2" offset-md="3" class="text-center">
         <v-tabs class="inverse" fixed-tabs>
-          <v-tab :to="{name: 'Login', query: {...$route.query}}" :replace="true">
+          <v-tab :to="{name: 'Login', query: {...route.query}}" :replace="true">
             Login
           </v-tab>
-          <v-tab :to="{name: 'Register', query: {...$route.query}}" :replace="true">
+          <v-tab :to="{name: 'Register', query: {...route.query}}" :replace="true">
             Register
           </v-tab>
-          <v-tab :to="{name: 'Forgot', query: {...$route.query}}" :replace="true">
+          <v-tab :to="{name: 'Forgot', query: {...route.query}}" :replace="true">
             Forgot
           </v-tab>
         </v-tabs>
@@ -19,58 +19,45 @@
   </v-container>
 </template>
 
-<script lang="ts">
-import {Component, mixins, toNative} from 'vue-facing-decorator'
-import AcFormContainer from '@/components/wrappers/AcFormContainer.vue'
-import VueHcaptcha from '@hcaptcha/vue3-hcaptcha'
-import Viewer from '@/mixins/viewer.ts'
-import AcForm from '@/components/wrappers/AcForm.vue'
-import AcBoundField from '@/components/fields/AcBoundField.ts'
-import {Auth} from '@/components/views/auth/mixins/Auth.ts'
+<script setup lang="ts">
+import {useAuth} from '@/components/views/auth/mixins/Auth.ts'
 import {watch} from 'vue'
+import {useRoute} from 'vue-router'
+import {useViewer} from '@/mixins/viewer.ts'
 
 declare type SyncFunc = (value: any) => void
+declare type AuthType = Omit<ReturnType<typeof useAuth>, 'loginHandler'|'sendToProfile'>
 
-function syncTo(...fields: Array<[string, string]>): SyncFunc {
-  function syncedField(this: any, value: any): void {
+const route = useRoute()
+const forms = useAuth()
+
+const syncTo = (...fields: Array<[keyof AuthType, string]>): SyncFunc => {
+  return (value: keyof ReturnType<typeof useAuth>): void => {
     for (const field of fields) {
       const formName = field[0]
       const fieldName = field[1]
       /* istanbul ignore if */
-      if (!this[formName]) {
+      if (!forms[formName]) {
         // Form isn't (yet) defined, can't sync.
         return
       }
-      this[formName].fields[fieldName].update(value, false)
+      forms[formName].fields[fieldName].update(value, false)
     }
   }
-
-  return syncedField
 }
 
-@Component({
-  components: {
-    AcBoundField,
-    AcForm,
-    AcFormContainer,
-    VueHcaptcha,
-  },
-})
-class AuthViews extends mixins(Viewer, Auth) {
-  public created() {
-    watch(() => this.loginForm.fields.email.value, syncTo(['registerForm', 'email'], ['forgotForm', 'email']).bind(this))
-    watch(() => this.registerForm.fields.email.value, syncTo(['loginForm', 'email'], ['forgotForm', 'email']).bind(this))
-    watch(() => this.forgotForm.fields.email.value, syncTo(['loginForm', 'email'], ['registerForm', 'email']).bind(this))
-    watch(() => this.loginForm.fields.password.value, syncTo(['registerForm', 'password']).bind(this))
-    watch(() => this.registerForm.fields.password.value, syncTo(['loginForm', 'password']).bind(this))
-    watch(() => this.loginForm.fields.order_claim.value, syncTo(['registerForm', 'order_claim']).bind(this))
-    watch(() => this.registerForm.fields.order_claim.value, syncTo(['loginForm', 'order_claim']).bind(this))
-    this.loginForm.fields.order_claim.update(this.$route.query.claim || '', false)
-    this.registerForm.fields.artist_mode.update(((this.$route.query.artist_mode && true) || false), false)
-    this.loginForm.clearErrors()
-    this.registerForm.clearErrors()
-  }
-}
 
-export default toNative(AuthViews)
+watch(() => forms.loginForm.fields.email.value, syncTo(['registerForm', 'email'], ['forgotForm', 'email']))
+watch(() => forms.registerForm.fields.email.value, syncTo(['loginForm', 'email'], ['forgotForm', 'email']))
+watch(() => forms.forgotForm.fields.email.value, syncTo(['loginForm', 'email'], ['registerForm', 'email']))
+watch(() => forms.loginForm.fields.password.value, syncTo(['registerForm', 'password']))
+watch(() => forms.registerForm.fields.password.value, syncTo(['loginForm', 'password']))
+watch(() => forms.loginForm.fields.order_claim.value, syncTo(['registerForm', 'order_claim']))
+watch(() => forms.registerForm.fields.order_claim.value, syncTo(['loginForm', 'order_claim']))
+forms.loginForm.fields.order_claim.update(route.query.claim || '', false)
+forms.registerForm.fields.artist_mode.update(((route.query.artist_mode && true) || false), false)
+forms.loginForm.clearErrors()
+forms.registerForm.clearErrors()
+// Used by tests.
+const {viewer} = useViewer()
 </script>
--- frontend/components/views/auth/mixins/Auth.ts
+++ frontend/components/views/auth/mixins/Auth.ts
@@ -1,8 +1,10 @@
 import {Component, mixins} from 'vue-facing-decorator'
 import {FormController} from '@/store/forms/form-controller.ts'
 import {User} from '@/store/profiles/types/User.ts'
-import Viewer from '@/mixins/viewer.ts'
+import Viewer, {useViewer} from '@/mixins/viewer.ts'
 import {singleQ} from '@/lib/lib.ts'
+import {useForm} from '@/store/forms/hooks.ts'
+import {useRoute, useRouter} from 'vue-router'
 
 @Component
 export class Auth extends mixins(Viewer) {
@@ -106,4 +108,104 @@ export class Auth extends mixins(Viewer) {
       },
     })
   }
+}
+
+export const useAuth = () => {
+  const router = useRouter()
+  const route = useRoute()
+  const {viewerHandler, viewer} = useViewer()
+  const loginForm = useForm('login', {
+    endpoint: '/api/profiles/login/',
+    fields: {
+      email: {
+        value: '',
+        validators: [{name: 'email'}, {name: 'required'}],
+      },
+      password: {
+        value: '',
+        validators: [{name: 'required'}],
+      },
+      token: {value: ''},
+      order_claim: {
+        value: '',
+        omitIf: '',
+      },
+    },
+  })
+  const registerForm = useForm('register', {
+    endpoint: '/api/profiles/register/',
+    fields: {
+      username: {
+        value: '',
+        validators: [{
+          name: 'username',
+          async: true,
+        }],
+      },
+      email: {
+        value: '',
+        validators: [
+          {
+            name: 'email',
+            async: true,
+          },
+        ],
+      },
+      password: {
+        value: '',
+        validators: [{name: 'required'}, {
+          name: 'password',
+          async: true,
+        }],
+      },
+      recaptcha: {
+        value: '',
+        validators: [{name: 'required'}],
+      },
+      artist_mode: {value: false},
+      registration_code: {value: ''},
+      mail: {value: true},
+      order_claim: {
+        value: '',
+        omitIf: '',
+      },
+    },
+  })
+  const forgotForm = useForm('forgot', {
+    endpoint: '/api/profiles/forgot-password/',
+    fields: {
+      email: {
+        value: '',
+        validators: [{name: 'required'}],
+      },
+    },
+  })
+
+  const loginHandler = (response: User) => {
+    viewerHandler.user.x = response
+    if ('next' in route.query) {
+      if (singleQ(route.query.next) === '/') {
+        return router.push({
+          name: 'Profile',
+          params: {username: response.username},
+        })
+      }
+      return router.push(singleQ(route.query.next))
+    } else if (route.name === 'Register') {
+      return sendToProfile()
+    }
+    return router.push({name: 'Home'})
+  }
+
+  const sendToProfile = () => {
+    return router.push(
+      {
+        name: 'Profile',
+        params: {username: viewer.value.username},
+        query: {editing: 'true'},
+      },
+    )
+  }
+
+  return {loginForm, registerForm, forgotForm, loginHandler, sendToProfile}
 }
